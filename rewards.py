207e3e44eb6964ffccefc7e9e1f112bb443c070a
studixxne <studioone@sogang.ac.kr>
2026-07-09 17:05:32 +0900

    feat: add answer length reward & penalty

--- rewards.py
+++ rewards.py
@@ -1,4 +1,5 @@
-from utils import extract_content, parse_format, parse_answer
+import math
+from utils import extract_content, parse_format, parse_answer, parse_tag
 
 @extract_content
 def format_reward(completions, **kwargs):
@@ -15,5 +16,43 @@ def answer_reward(completions, answer, **kwargs):
             rewards.append(1.0)
         else:
             rewards.append(0.0)
-            
     return rewards
+
+@extract_content
+def answer_length_reward(completions, **kwargs):
+    rewards = []
+    target_length = 200
+    max_reward = 0.5
+
+    for c in completions:
+        _, answer = parse_tag(c)
+        reward = 0
+        if answer:
+            answer_length = len(answer)
+            if answer_length >= target_length:
+                reward = max_reward
+            else:
+                reward = max_reward * math.sin((answer_length / target_length) * (math.pi / 2))
+        rewards.append(reward)
+    
+    return rewards
+
+@extract_content
+def answer_length_penalty(completions, **kwargs):
+    penalties = []
+    target_length = 200
+    max_penalty = -0.5
+
+    for c in completions:
+        _, answer = parse_tag(c)
+        penalty = max_penalty
+        if answer:
+            answer_length = len(answer)
+            if answer_length >= target_length:
+                penalty = 0.0
+            else:
+                penalty = max_penalty * (1 - math.sin((answer_length / target_length) * (math.pi / 2)))
+
+        penalties.append(penalty)
+
+    return penalties
