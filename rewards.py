2d22a75ddff5f40b802a4d4998380cbc8f501ee3
studixxne <studioone@sogang.ac.kr>
2026-07-09 14:18:11 +0900

    refactor: remove logging and detach parsing function

--- rewards.py
+++ rewards.py
@@ -1,35 +1,19 @@
-import re
-import json
-
-from utils import extract_content
+from utils import extract_content, parse_format, parse_answer
 
 @extract_content
 def format_reward(completions, **kwargs):
-    pattern = re.compile(r"^<reasoning>.*?</reasoning>\s*<answer>.*?</answer>$", re.DOTALL)
-    return [0.2 if pattern.match(pred) else 0.0 for pred in completions]
+    return [0.2 if parse_format(pred) else 0.0 for pred in completions]
 
 @extract_content
 def answer_reward(completions, answer, **kwargs):
-    pattern = re.compile(r"####\s*(\d+)")
-
     rewards = []
-    for pred, a in zip(completions, answer):
-        pred_match = pattern.findall(pred)
-        ans_match = pattern.findall(a)
+    for c, a in zip(completions, answer):
+        pred = parse_answer(c)
+        ans = parse_answer(a)
 
-        if (pred_match and ans_match) and int(pred_match[-1]) == int(ans_match[-1]):
+        if pred and pred == ans:
             rewards.append(1.0)
         else:
             rewards.append(0.0)
-
+            
     return rewards
-
-# 단순 대화 기록 logging
-@extract_content
-def logging_reward(completions, answer, **kwargs):
-    with open("all_completions_log.jsonl", "a", encoding="utf-8") as f:
-        for c, a in zip(completions, answer):
-            log_data = {"completion": c, "answer": a}
-            f.write(json.dumps(log_data, ensure_ascii=False) + '\n')
-
-    return [0.0] * len(completions)
